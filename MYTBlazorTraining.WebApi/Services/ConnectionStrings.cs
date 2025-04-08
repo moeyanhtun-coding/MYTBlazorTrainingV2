9ec1337a2c49ce7520b15ec2ad8527de5932a22b
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-08 20:19:07 +0630

    [Add] Services

---
+++ MYTBlazorTraining.WebApi/Services/ConnectionStrings.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace MYTBlazorTraining.WebApi.Services
+{
+    public class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "MYTDotNetCore",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true,
+        };
+    }
+}
