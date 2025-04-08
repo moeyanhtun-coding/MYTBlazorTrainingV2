d27c4c0ab5f9275ec0e2e6290f7632759c45324a
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-08 20:17:42 +0630

    [Add] AppDbContext

---
+++ MYTBlazorTraining.WebApi/Db/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using MYTBlazorTraining.WebApi.Models;
+namespace MYTBlazorTraining.WebApi.Db
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
