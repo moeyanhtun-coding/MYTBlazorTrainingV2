c9b91c43d4fca3dc0091ff9193b0c5e69149510e
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-09 13:02:47 +0630

    [Add] Cors Enable

--- MYTBlazorTraining.WebApi/Program.cs
+++ MYTBlazorTraining.WebApi/Program.cs
@@ -1,4 +1,16 @@
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("https://localhost:7279",
+                "http://localhost:5139");
+        });
+});
 
 // Add services to the container.
 
@@ -17,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
