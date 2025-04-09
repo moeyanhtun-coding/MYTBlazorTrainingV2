7053919d3d7c18ba2052e16fec1e1b961e29f650
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-09 22:50:07 +0630

    [Add] Bootstrap JS and modal helper functions

---
+++ MYTBlazorTraining.Wasm2/wwwroot/js/bootstrap-helper.js
@@ -0,0 +1,10 @@
+﻿export function showModal(id) {
+    const modal = new bootstrap.Modal(document.querySelector(id));
+    modal.show();
+}
+
+export function hideModal(id) {
+    const modalEl = document.querySelector(id);
+    const modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
+}
