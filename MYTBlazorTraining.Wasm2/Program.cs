94424970654f26402c278b19ca1158a7aecf46ea
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-09 13:02:33 +0630

    [Add] Backend Api

--- MYTBlazorTraining.Wasm2/Program.cs
+++ MYTBlazorTraining.Wasm2/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MYTBlazorTraining.Wasm2;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
