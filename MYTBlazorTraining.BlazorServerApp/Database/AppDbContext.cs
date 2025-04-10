757885247e4f7f04561cf3eb115c4a666fc2fa6f
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-10 13:26:41 +0630

    [Add] AppDbContext for BlogModel entity management

---
+++ MYTBlazorTraining.BlazorServerApp/Database/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MYTBlazorTraining.BlazorServerApp.Models;
+
+namespace MYTBlazorTraining.BlazorServerApp.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+
+        public DbSet<BlogModel> Blogs {get; set;}
+    }
+}
