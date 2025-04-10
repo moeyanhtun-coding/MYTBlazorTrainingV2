afd437e346d462fc6b4cfea5cba4e0ec7acbef9d
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-10 13:33:14 +0630

    [Add] Set up Blazor Server app with EF Core and Services

---
+++ MYTBlazorTraining.BlazorServerApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using MYTBlazorTraining.BlazorServerApp.Data;
+using MYTBlazorTraining.BlazorServerApp.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("DbConnection")!;
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
