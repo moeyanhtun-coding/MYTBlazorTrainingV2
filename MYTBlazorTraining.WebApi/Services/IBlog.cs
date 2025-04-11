fe417cd94f3d53556580c11b2a454571ef4d0071
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 11:55:00 +0630

    [Add] IBlog interface for blog management operations

---
+++ MYTBlazorTraining.WebApi/Services/IBlog.cs
@@ -0,0 +1,13 @@
+﻿using MYTBlazorTraining.WebApi.Models.Blog;
+
+namespace MYTBlazorTraining.WebApi.Services
+{
+    public interface IBlog
+    {
+        Task<BlogListResponseModel> GetBlogListAsync();
+        Task<BlogEditResponseModel> GetBlogByIdAsync(int id);
+        Task<BlogCreateResponseModel> CreateBlogAsync(BlogModel blogModel);
+        Task<BlogUpdateResponseModel> UpdateBlogAsync(int id, BlogModel blogModel);
+        Task<BlogDeleteResponseModel> DeleteBlogAsync(int id);
+    }
+}
