24bb031092257f7d58e745e089c7ff604dbfea9e
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 11:53:49 +0630

    [Add] ModularServices for service registration

---
+++ MYTBlazorTraining.WebApi/ModularServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MYTBlazorTraining.WebApi.Db;
+using MYTBlazorTraining.WebApi.Repository;
+using MYTBlazorTraining.WebApi.Services;
+
+namespace MYTBlazorTraining.WebApi
+{
+    public static class ModularServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext(configuration);
+            services.AddRepository();
+            return services;
+        }
+
+        public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DbConnection")!;
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString),
+                ServiceLifetime.Transient,
+                ServiceLifetime.Transient
+            );
+            return services;
+        }
+
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IBlog, BlogRepo>();
+            services.AddScoped<IAuth, AuthRepo>();
+            return services;
+
+        }
+    }
+}
