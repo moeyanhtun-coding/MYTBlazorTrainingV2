0e946bfb91ddc3f47451b7b0f264afaf0d800bb5
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 13:28:34 +0630

    [Add] authentication methods to AuthController

--- MYTBlazorTraining.WebApi/Controllers/AuthController.cs
+++ MYTBlazorTraining.WebApi/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using MYTBlazorTraining.WebApi.Models.Login;
 using MYTBlazorTraining.WebApi.Models.Register;
+using MYTBlazorTraining.WebApi.Services;
 
 namespace MYTBlazorTraining.WebApi.Controllers
 {
@@ -9,5 +11,26 @@ namespace MYTBlazorTraining.WebApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuth _auth;
+
+        public AuthController(IAuth auth)
+        {
+            _auth = auth;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<UserLoginResponseModel>> UserLogin(UserLoginModel user)
+        {
+            var result = await _auth.UserLoginAsync(user);  
+            if(result.Flag) return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserRegisterResponseModel>> UserRegister(UserRegisterModel user) 
+        {
+            var result = await _auth.UserRegisterAsync(user);
+            if (result.Flag) return Ok(result);
+            return BadRequest(result);
+        }
     }
 }
