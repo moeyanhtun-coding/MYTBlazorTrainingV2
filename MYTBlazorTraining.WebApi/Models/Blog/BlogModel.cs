c9a2ffbf40a19dfe4eb6d42840a2e284030f83c7
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 11:57:11 +0630

    [Add] blog models and response models for API

---
+++ MYTBlazorTraining.WebApi/Models/Blog/BlogModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYTBlazorTraining.WebApi.Models.Blog
+{
+    public class BlogModel
+    {
+        [Required]
+        public string? BlogTitle { get; set; }
+        [Required]
+        public string? BlogAuthor { get; set; }
+        [Required]
+        public string? BlogContent { get; set; }
+    }
+}
---
+++ MYTBlazorTraining.WebApi/Models/Blog/BlogResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using MYTBlazorTraining.WebApi.Db;
+
+namespace MYTBlazorTraining.WebApi.Models.Blog
+{
+    public record BlogListResponseModel(bool Success, List<TblBlog> Blogs);
+    public record BlogEditResponseModel(bool Success, TblBlog Blog, string Message = null);
+    public record BlogCreateResponseModel(bool Success, string Message = null);
+    public record BlogUpdateResponseModel(bool Success, string Message = null);
+    public record BlogDeleteResponseModel(bool Success, string Message = null);
+}
