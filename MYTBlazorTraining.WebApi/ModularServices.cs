0722e9562bec28cee5b338b849012946b97bd6cf
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 16:43:12 +0630

    [Add] JWT authentication support in ModularServices.cs

--- MYTBlazorTraining.WebApi/ModularServices.cs
+++ MYTBlazorTraining.WebApi/ModularServices.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MYTBlazorTraining.WebApi.Db;
 using MYTBlazorTraining.WebApi.Repository;
 using MYTBlazorTraining.WebApi.Services;
+using System.Text;
 
 namespace MYTBlazorTraining.WebApi
 {
     public static class ModularServices
     {
-        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration,
+            WebApplicationBuilder builder)
         {
             services.AddDbContext(configuration);
             services.AddRepository();
+            services.AddAuthentication(builder, configuration);
             return services;
         }
 
@@ -21,6 +26,26 @@ namespace MYTBlazorTraining.WebApi
                 ServiceLifetime.Transient,
                 ServiceLifetime.Transient
             );
+            return services;
+        }
+
+        public static IServiceCollection AddAuthentication(this IServiceCollection services,
+          WebApplicationBuilder builder, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["JWT:Issuer"],
+                ValidAudience = builder.Configuration["JWT:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]!))
+            });
             return services;
         }
 
