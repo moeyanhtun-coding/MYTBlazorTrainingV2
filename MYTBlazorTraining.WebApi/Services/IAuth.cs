2fa3adc3858659271869562600b8451b467600e0
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 12:01:14 +0630

    [Modifty] Remove ConnectionStrings and add IAuth interface

---
+++ MYTBlazorTraining.WebApi/Services/IAuth.cs
@@ -0,0 +1,11 @@
+﻿using MYTBlazorTraining.WebApi.Models.Login;
+using MYTBlazorTraining.WebApi.Models.Register;
+
+namespace MYTBlazorTraining.WebApi.Services
+{
+    public interface IAuth
+    {
+        Task<UserLoginResponseModel> UserLoginAsync(UserLoginModel userLoginModel);
+        Task<UserRegisterResponseModel> UserRegisterAsync(UserRegisterModel userRegisterModel);
+    }
+}
