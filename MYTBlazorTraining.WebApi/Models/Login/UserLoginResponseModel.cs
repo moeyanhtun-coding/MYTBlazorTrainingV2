4ecd761e0979d312a1116eee9398b3da8edb7afa
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 13:36:20 +0630

    [Make] Token property nullable in UserLoginResponseModel

--- MYTBlazorTraining.WebApi/Models/Login/UserLoginResponseModel.cs
+++ MYTBlazorTraining.WebApi/Models/Login/UserLoginResponseModel.cs
@@ -1,4 +1,4 @@
 ﻿namespace MYTBlazorTraining.WebApi.Models.Login
 {
-    public record UserLoginResponseModel (bool Flag, string Message, string Token);
+    public record UserLoginResponseModel (bool Flag, string Message, string? Token = null);
 }
