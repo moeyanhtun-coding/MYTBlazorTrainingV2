09bf0ceb2a8dcd4043d5da00c82cd7cbc5e1cdf4
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 12:00:08 +0630

    [Add] AuthController with routing and necessary usings

---
+++ MYTBlazorTraining.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MYTBlazorTraining.WebApi.Models.Login;
+using MYTBlazorTraining.WebApi.Models.Register;
+
+namespace MYTBlazorTraining.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
