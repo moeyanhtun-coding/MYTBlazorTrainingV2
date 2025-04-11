395bc0795ae340f09fedffe261619e3c730a6dc8
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 11:58:34 +0630

    [Add] user registration models and response structure

---
+++ MYTBlazorTraining.WebApi/Models/Register/UserRegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MYTBlazorTraining.WebApi.Models.Register
+{
+    public class UserRegisterModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Email is required"), EmailAddress]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Password is required"), MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+        public string? Password { get; set; }
+        [Required(ErrorMessage = "Confirm Password is required"),
+            MinLength(6, ErrorMessage = "Confirm Password must be at least 6 characters long"),
+            Compare("Password", ErrorMessage = "Password and Confirm Password do not match")
+            ]
+        public string? ConfirmPassword { get; set; }
+    }
+}
---
+++ MYTBlazorTraining.WebApi/Models/Register/UserRegisterResponseModel.cs
@@ -0,0 +1,4 @@
+﻿namespace MYTBlazorTraining.WebApi.Models.Register
+{
+    public record UserRegisterResponseModel(bool Flag, string Message = null);
+}
