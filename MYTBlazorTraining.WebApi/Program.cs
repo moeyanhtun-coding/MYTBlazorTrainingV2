b47a27b4683b936ecd7ded152de102c53a272242
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 16:43:48 +0630

    Add Swagger and JWT authentication to Program.cs

--- MYTBlazorTraining.WebApi/Program.cs
+++ MYTBlazorTraining.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using MYTBlazorTraining.WebApi;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -19,8 +20,42 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddServices(builder.Configuration);
+builder.Services.AddSwaggerGen(
+    swagger =>
+    {
+        swagger.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "ASP.NET Core 8 Web API",
+            Description = "ASP.NET Core 8 Web API with JWT Authentication"
+        });
+        swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+    }
+    );
+
+builder.Services.AddServices(builder.Configuration, builder);
 
 var app = builder.Build();
 
@@ -35,6 +70,7 @@ app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
