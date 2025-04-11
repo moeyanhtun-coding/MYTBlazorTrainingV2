97294800342d9ac5da6f7b8a0adc9c3a38036018
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 13:35:54 +0630

    [Implement] user authentication and registration in AuthRepo

--- MYTBlazorTraining.WebApi/Repository/AuthRepo.cs
+++ MYTBlazorTraining.WebApi/Repository/AuthRepo.cs
@@ -1,4 +1,6 @@
-﻿using MYTBlazorTraining.WebApi.Models.Login;
+﻿using Microsoft.EntityFrameworkCore;
+using MYTBlazorTraining.WebApi.Db;
+using MYTBlazorTraining.WebApi.Models.Login;
 using MYTBlazorTraining.WebApi.Models.Register;
 using MYTBlazorTraining.WebApi.Services;
 
@@ -6,14 +8,43 @@ namespace MYTBlazorTraining.WebApi.Repository
 {
     public class AuthRepo : IAuth
     {
-        public Task<UserLoginResponseModel> UserLoginAsync(UserLoginModel userLoginModel)
+        private readonly AppDbContext _contenxt;
+
+        public AuthRepo(AppDbContext contenxt)
         {
-            throw new NotImplementedException();
+            _contenxt = contenxt;
         }
 
-        public Task<UserRegisterResponseModel> UserRegisterAsync(UserRegisterModel userRegisterModel)
+        public async Task<UserLoginResponseModel> UserLoginAsync(UserLoginModel userLoginModel)
         {
-            throw new NotImplementedException();
+            var getUser = await FindUserByEmail(userLoginModel.Email!);
+            if (getUser is null)
+                return new UserLoginResponseModel(false, "User Not Found");
+            var checkPassword = BCrypt.Net.BCrypt.Verify(userLoginModel.Password, getUser.Password);
+            if (!checkPassword)
+                return new UserLoginResponseModel(false, "Credentials are not valid");
+            return new UserLoginResponseModel(true, "Login Successfully");
         }
+
+        public async Task<UserRegisterResponseModel> UserRegisterAsync(UserRegisterModel userRegisterModel)
+        {
+            var getUser = await FindUserByEmail(userRegisterModel.Email!);
+            if (getUser is not null) return new UserRegisterResponseModel(false, "User Already Exist");
+            var user = new TblUser
+            {
+                Name = userRegisterModel.Name,
+                Email = userRegisterModel.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(userRegisterModel.Password)
+            };
+            await _contenxt.Users.AddAsync(user);
+            var result = await _contenxt.SaveChangesAsync();
+            if (result is 0) return new UserRegisterResponseModel(false, "User Not Created");
+            return new UserRegisterResponseModel(true, "User Created Successfully");
+
+        }
+
+        private async Task<TblUser?> FindUserByEmail(string email) => 
+            await _contenxt.Users.AsNoTracking().FirstOrDefaultAsync(x => x.Email == email);
     }
 }
+
