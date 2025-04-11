770bd31d1b32bb4f4dfadb42d223f2ba50fe2524
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 12:02:58 +0630

    [Add] AuthRepo class for user authentication

---
+++ MYTBlazorTraining.WebApi/Repository/AuthRepo.cs
@@ -0,0 +1,19 @@
+﻿using MYTBlazorTraining.WebApi.Models.Login;
+using MYTBlazorTraining.WebApi.Models.Register;
+using MYTBlazorTraining.WebApi.Services;
+
+namespace MYTBlazorTraining.WebApi.Repository
+{
+    public class AuthRepo : IAuth
+    {
+        public Task<UserLoginResponseModel> UserLoginAsync(UserLoginModel userLoginModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserRegisterResponseModel> UserRegisterAsync(UserRegisterModel userRegisterModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
